f1698aa526ccf0f8195ea65779471fb3e4bb764d
Pablo <sebastianpablo925@gmail.com>
2024-02-24 19:23:53 -0500

    se agrega el proyecto

---
+++ Control_comidas/Login.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Control_comidas.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" ></script>
+    <link href="Recursos/CSS/Estilos.css" rel="stylesheet" />
+    <title>LOGIN</title>
+</head>
+<body class="bg-light">
+    <div class="wrapper">
+        <div class="formcontent">
+            <form id="formulario_login" runat="server">
+                <div class="form-control">
+                    <div class="col-md-6 text-center mb-5">
+                        <asp:Label class="h2" ID="lblBienvenida" runat="server" Text="Bienvenido Scout"></asp:Label>
+                    </div>
+                    <div>
+                        <asp:Label ID="lblUsuario" runat="server" Text="Usuario"></asp:Label>
+                        <asp:TextBox ID="txtUsuario" CssClass="form-control" runat="server" placeholder ="Nombre de Usuario"></asp:TextBox>
+                    </div>
+                    <div>
+                        <asp:Label ID="lblPassword" runat="server" Text="Password"></asp:Label>
+                        <asp:TextBox ID="txtPassword" CssClass="form-control" runat="server" placeholder ="Password"></asp:TextBox>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <asp:Button ID="btnIngresar" CssClass="btn btn-outline-success" runat="server" Text="Ingresar" />
+
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ Control_comidas/Login.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Control_comidas
+{
+
+
+    public partial class Login
+    {
+
+        /// <summary>
+        /// Control formulario_login.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm formulario_login;
+
+        /// <summary>
+        /// Control lblBienvenida.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblBienvenida;
+
+        /// <summary>
+        /// Control lblUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsuario;
+
+        /// <summary>
+        /// Control txtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuario;
+
+        /// <summary>
+        /// Control lblPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPassword;
+
+        /// <summary>
+        /// Control txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+
+        /// <summary>
+        /// Control btnIngresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIngresar;
+    }
+}
